c57bee3563c77c33b3feb3fb02895ff2c1afcb8d
dasha akhten <dasha.akhten@gmail.com>
2020-04-13 19:28:41 -0700

    seed tables

---
+++ seeds/seed.days.sql
@@ -0,0 +1,11 @@
+INSERT INTO days (id, title )
+VALUES
+    (1, 'Monday'),
+    (2, 'Tuesday'),
+    (3, 'Wednesday'),
+    (4, 'Thursday'),
+    (5, 'Friday'),
+                    (6, 'Saturday'),
+                        (7, 'Sunday');
+
+              
--- seeds/seed.food.sql
+++ seeds/seed.food.sql
@@ -1,8 +1,11 @@
 INSERT INTO food (id, content, days_id)
 VALUES
-    (1, 'banana', 1),
-    (2, 'broccoli', 2),
-    (3, 'bread', 3)
+    (1, 'bread', 2),
+    (2, 'tomatoes', 2),
+    (3, 'brocolli', 5),
+    (4, 'eggs', 3),
+(5, 'chicken', 2),
+(6, 'vodka', 2),
+(7, 'tacos', 4),
+(8, 'lasagna', 4);
     
-
-
