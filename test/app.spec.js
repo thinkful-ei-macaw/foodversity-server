2e0763e7a5a85421e7c4fddf4953bce236290caf
dasha akhten <dasha.akhten@gmail.com>
2020-04-23 13:05:40 -0700

    testing

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,7 +1,34 @@
 const app = require("../src/app");
+const chai = require("chai");
+const supertest = require("supertest");
+const expect = chai.expect;
 
 describe("App", () => {
   it('GET /responds with 200 containing "I am serving, I am a server!"', () => {
     return supertest(app).get("/").expect(200, "I am serving, I am a server!");
   });
 });
+
+describe("Loading the days page", () => {
+  it("should return 200 from GET /days", () => {
+    return supertest(app).get("/days").expect(200);
+  });
+});
+
+// describe("GET /days endpoint", () => {
+//   it("should return 200", () => {
+//     const days = {
+//       id: 1,
+//       title: "monday",
+//     };
+//     return supertest(app)
+//       .get("/days")
+//       .days(days)
+//       .expect(200)
+//       .expect("Content-Type", /json/)
+//       .then((res) => {
+//         expect(res.body).to.be.an("array");
+//         expect(res.body[0]).to.be.an("object");
+//       });
+//   });
+// });
