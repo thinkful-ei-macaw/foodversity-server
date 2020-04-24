9b9f7a99414ceefca1e171b862495ef548ffffc0
dasha akhten <dasha.akhten@gmail.com>
2020-04-24 09:50:17 -0700

    test

--- test/app.spec.js
+++ test/app.spec.js
@@ -7,27 +7,3 @@ describe("App", () => {
     return supertest(app).get("/").expect(200, "I am serving, I am a server!");
   });
 });
-
-describe("Loading the days page", () => {
-  it("should return 200 from GET /days", () => {
-    return supertest(app).get("/days").expect(200, days);
-  });
-});
-
-// describe("GET /days endpoint", () => {
-//   it("should return 200", () => {
-//     const days = {
-//       id: 1,
-//       title: "monday",
-//     };
-//     return supertest(app)
-//       .get("/days")
-//       .days(days)
-//       .expect(200)
-//       .expect("Content-Type", /json/)
-//       .then((res) => {
-//         expect(res.body).to.be.an("array");
-//         expect(res.body[0]).to.be.an("object");
-//       });
-//   });
-// });
--- test/days-endpoints.spec.js
+++ test/days-endpoints.spec.js
@@ -4,9 +4,9 @@ const app = require("../src/app");
 
 describe("GET /days/:id", () => {
   context("Given no days in the database", () => {
-    it("responds with 500", () => {
+    it("responds with 401", () => {
       const id = 123456;
-      return supertest(app).get(`/days/${id}`).expect(500);
+      return supertest(app).get(`/days/${id}`).expect(401);
     });
   });
 });
